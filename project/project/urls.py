78c55f9b89cf0becbf89525d66f956febbc7405a
GoshiX <90464815+GoshiX@users.noreply.github.com>
2021-12-02 01:04:08 +0300

    full work intro

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,4 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('regestration.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
